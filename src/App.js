354190b5a10f50b0a5e5936817275fe12b1974f5
jameshamann <jameshamann@me.com>
2016-12-11 19:57:10 +0000

    working on hiding results

--- src/App.js
+++ src/App.js
@@ -5,15 +5,6 @@ import './App.css';
 
 class App extends Component {
 
-  getInitialState() {
-    return { showResults: false };
-  }
-
-  onSubmit() {
-    return { showResults: true };
-  }
-
-
   constructor(props) {
     super(props);
     this.state = {value: ''};
@@ -45,18 +36,6 @@ class App extends Component {
     event.preventDefault();
   }
 
-  showResults() {
-    return (
-    <div className="Title-info">
-      <img src={this.state.poster} alt="Title Poster"></img>
-      <p>Title: {this.state.title}</p>
-      <p>Released: {this.state.released}</p>
-      <p>Metacritic Rating: {this.state.metacritic}</p>
-      <p>IMDB Rating: {this.state.imdb}</p>
-    </div>
-  );
-}
-
   render() {
     return (
       <div className="App">
@@ -71,6 +50,13 @@ class App extends Component {
        </label>
         <input type="submit" value="Submit" />
      </form>
+    </div>
+    <div className="Title-info">
+      <img src={this.state.poster} alt="Title Poster"></img>
+      <p>Title: {this.state.title}</p>
+      <p>Released: {this.state.released}</p>
+      <p>Metacritic Rating: {this.state.metacritic}</p>
+      <p>IMDB Rating: {this.state.imdb}</p>
     </div>
   </div>
     );
