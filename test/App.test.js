27497ed1c446931771287c54fefb115b7de864d7
jameshamann <jameshamann@me.com>
2016-12-27 17:04:41 +0000

    adds test to ensure search results are rendered

--- test/App.test.js
+++ test/App.test.js
@@ -2,9 +2,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import SearchResults from '../src/SearchResults.js';
 import App from '../src/App.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders the search results', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(SearchResults).length).toEqual(1);
 });
