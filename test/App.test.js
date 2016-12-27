dbecd4023c7fb5525c40a381850346af93200295
jameshamann <jameshamann@me.com>
2016-12-27 16:44:38 +0000

    ensures jsdom is being used and default test now passes

--- test/App.test.js
+++ test/App.test.js
@@ -1,10 +1,10 @@
-'use strict';
+'esversion: 6';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../src/App.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App/>, div)
+  ReactDOM.render(<App />, div);
 });
