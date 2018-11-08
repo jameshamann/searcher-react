0baec6d4c2a1ac37556c78423ac4cfb8844c0628
James Hamann <jameshamann@me.com>
2018-11-08 18:28:41 +0000

    builds for production and edits README

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -22,7 +22,7 @@ class SearchResults extends Component {
     var results;
     var self = this;
     console.log(self);
-    fetch('http://www.omdbapi.com/?apikey=' + process.env.REACT_APP_OMDB_API_KEY + '&tomatoes=true&t=' + this.state.value)
+    fetch('http://www.omdbapi.com/?apikey=39a24e30&tomatoes=true&t=' + this.state.value)
         .then(function(res) {
             return res.json();
         }).then(function(json) {
